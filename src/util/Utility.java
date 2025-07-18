4bea48b2d9fab4a2ad2c7963a6352caee31b943a
nishant-bankar <nishantbankar03@gmail.com>
2025-07-18 13:17:03 +0530

    Add files via upload

---
+++ src/util/Utility.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Utility {
+	public static String deffaul_com3 = "D:\\MPApplication\\Eddysorter\\Default_COM3\\DEFAULT_PARA_COM3.txt";
+	public static String no_of_dot = "D:\\MPApplication\\Eddysorter\\Default_COM4\\no_dot_count.txt";
+	
+	public static String application_logo = "D:\\MPApplication\\Eddysorter\\Image\\anpl_logo_5.png";
+	public static String company_logo = "D:\\MPApplication\\Eddysorter\\Image\\anpl_logo_5.png";
+	
+	public static String graph_img = "D:\\MPApplication\\Bills\\";
+	public static String reportheading_image = "D:\\MPApplication\\Eddysorter\\Image\\anpl_logo99.png";
+	
+	public static String gifimag = "D:\\MPApplication\\Eddysorter\\Image\\tenor.gif";
+	
+	
+
+	}
